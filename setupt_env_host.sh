1f5862a6fa39b6514ab01c1332cad28ed4e82acc
Ninh <41134638+ninhnguyennhut@users.noreply.github.com>
2018-12-10 00:18:57 +0700

    Update setupt_env_host.sh

--- setupt_env_host.sh
+++ setupt_env_host.sh
@@ -1,5 +1,8 @@
+#ubuntu 18 LTS
+
 sudo apt-get install git cmake vim autoconf -y
 sudo apt install python-minimal -y
 sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm -y
 sudo apt-get install libcurl-ocaml-dev libmosquitto-dev libmosquittopp-dev libusb-1.0-0-dev libssl-ocaml-dev -y
+sudo apt-get install bison flex swig -y
 
