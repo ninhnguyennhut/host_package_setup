421e09afebe4b045d96d2906aa1185483c036379
Ninh <41134638+ninhnguyennhut@users.noreply.github.com>
2018-12-22 12:53:54 +0700

    Update setupt_env_host.sh

--- setupt_env_host.sh
+++ setupt_env_host.sh
@@ -6,3 +6,8 @@ sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib buil
 sudo apt-get install libcurl-ocaml-dev libmosquitto-dev libmosquittopp-dev libusb-1.0-0-dev libssl-ocaml-dev -y
 sudo apt-get install bison flex swig -y
 
+
+yocto package
+
+sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm
+
