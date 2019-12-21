5e5657cc4a6c4c337bf0af769cfb2032f3d698ea
Ninh <41134638+ninhnguyennhut@users.noreply.github.com>
2019-12-21 11:56:40 +0700

    Update setupt_env_host.sh

--- setupt_env_host.sh
+++ setupt_env_host.sh
@@ -6,11 +6,25 @@ sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib buil
 sudo apt-get install libcurl-ocaml-dev libmosquitto-dev libmosquittopp-dev libusb-1.0-0-dev libssl-ocaml-dev -y
 sudo apt-get install bison flex swig -y
 
+crossbuild qt5
+
+https://wiki.qt.io/RaspberryPi2EGLFS
+
+sudo apt-get install libudev-dev libinput-dev libts-dev libxcb-xinerama0-dev libxcb-xinerama0 libx11-dev
 
 #yocto package install
 
 sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm
 
+Ubuntu 18.04.2 tested build opencv
+
+- sudo apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libjpeg-dev libpng-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer-ocaml-dev libgstreamer-plugins-base1.0-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
+
+- cd opencv-3.4.6
+- mkdir release
+- cd release
+
+-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_IPP=OFF -D OPENCV_EXTRA_MODULES_PATH=/home/fanning/workspace/linux/opencv_contrib-3.4.6/modules /home/fanning/workspace/linux/opencv-3.4.6
 
 #stlink stm32f4 black board
 SWDIO --- Pin 7, PA13/TMS/JTMS_SWDIO
