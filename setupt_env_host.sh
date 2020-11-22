0408e96c4a89687f87f541d406eb549566a25fdf
Ninh <41134638+ninhnguyennhut@users.noreply.github.com>
2020-11-22 22:28:21 +0700

    Update setupt_env_host.sh

--- setupt_env_host.sh
+++ setupt_env_host.sh
@@ -4,7 +4,8 @@ sudo apt-get install git cmake vim autoconf -y
 sudo apt install python-minimal -y
 sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm -y
 sudo apt-get install libcurl-ocaml-dev libmosquitto-dev libmosquittopp-dev libusb-1.0-0-dev libssl-ocaml-dev -y
-sudo apt-get install bison flex swig -y
+sudo apt-get install bison flex swig libtool -y
+sudo apt-get install uuid-dev libacl1-dev liblzo2-dev libzstd-dev
 
 crossbuild qt5
 
