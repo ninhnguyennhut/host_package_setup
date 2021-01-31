7977f8996423dc58fcaf6c1452d374bbca84c7d5
Ninh <41134638+ninhnguyennhut@users.noreply.github.com>
2021-01-31 10:35:04 +0700

    Update setupt_env_host.sh

--- setupt_env_host.sh
+++ setupt_env_host.sh
@@ -6,6 +6,7 @@ sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib buil
 sudo apt-get install libcurl-ocaml-dev libmosquitto-dev libmosquittopp-dev libusb-1.0-0-dev libssl-ocaml-dev -y
 sudo apt-get install bison flex swig libtool curl -y
 sudo apt-get install uuid-dev libacl1-dev liblzo2-dev libzstd-dev lib32z1 device-tree-compiler minicom
+sudo apt-get install libncurses5-dev libncursesw5-dev
 
 install golang
 wget https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz
